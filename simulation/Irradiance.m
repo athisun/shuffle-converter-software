8be69f46c697fe0aef8279a71b92088e93d65cd7
Isaac <git@eggsampler.com>
2019-09-22 09:15:52 +1000

    Add matlab simulink model

---
+++ simulation/Irradiance.m
@@ -0,0 +1,37 @@
+%double click on the VIdata spreadsheet through MATLAB, when it opens
+%change output type to "numeric matrix"
+%choose the irradiance spreadsheet tab at the bottom so you're only
+%looking at that one tab, and then click "import"
+%confirm this has imported a numeric matrix called "VIdataS1" that is 
+%3045x32
+
+I1 = VIdataS1(:,3:4);
+I2 = VIdataS1(:,([3 5]));
+I3 = VIdataS1(:,([3 6]));
+I4 = VIdataS1(:,([3 7]));
+I5 = VIdataS1(:,([3 8]));
+I6 = VIdataS1(:,([3 9]));
+I7 = VIdataS1(:,([3 10]));
+I8 = VIdataS1(:,([3 11]));
+I9 = VIdataS1(:,([3 12]));
+I10 = VIdataS1(:,([3 13]));
+I11 = VIdataS1(:,([3 14]));
+I12 = VIdataS1(:,([3 15]));
+I13 = VIdataS1(:,([3 16]));
+I14 = VIdataS1(:,([3 17]));
+I15 = VIdataS1(:,([3 18]));
+I16 = VIdataS1(:,([3 19]));
+I17 = VIdataS1(:,([3 20]));
+I18 = VIdataS1(:,([3 21]));
+I19 = VIdataS1(:,([3 22]));
+I20 = VIdataS1(:,([3 23]));
+I21 = VIdataS1(:,([3 24]));
+I22 = VIdataS1(:,([3 25]));
+I23 = VIdataS1(:,([3 26]));
+I24 = VIdataS1(:,([3 27]));
+I25 = VIdataS1(:,([3 28]));
+I26 = VIdataS1(:,([3 29]));
+I27 = VIdataS1(:,([3 30]));
+I28 = VIdataS1(:,([3 31]));
+I29 = VIdataS1(:,([3 32]));
+
